f464604576a0402d0ead70d4c0db8c2d483a8432
sweta <swetanakadi@gmail.com>
2024-08-29 13:30:21 +0530

    Adds test case that fails to test CICD

--- entrypoints/dev-start.sh
+++ entrypoints/dev-start.sh
@@ -4,4 +4,10 @@ python3 manage.py makemigrations --settings=setup.settings.dev
 python3 manage.py migrate --settings=setup.settings.dev
 echo "Running tests with development settings"
 python3 manage.py test myapp --settings=setup.settings.dev
-python3 manage.py runserver --settings=setup.settings.dev 0.0.0.0:8000
+if [ $? -eq 0 ]; then
+  echo "All test case passed"
+  python3 manage.py runserver --settings=setup.settings.dev 0.0.0.0:8000
+else
+  echo "Tests failed. Not starting django server"
+  exit 1
+fi
