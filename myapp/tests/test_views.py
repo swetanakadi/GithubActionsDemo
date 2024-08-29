376ca7ac6c66b45fa7917a4a7c0c4896a472fc6b
sweta <swetanakadi@gmail.com>
2024-08-29 13:24:28 +0530

    Removes tests.py file and adds tests package

---
+++ myapp/tests/test_views.py
@@ -0,0 +1,19 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+import os
+
+print(f"Current file: {__file__}")
+print(f"Current working directory: {os.getcwd()}")
+
+
+class TesDemoView(APITestCase):
+    def setUp(self) -> None:
+        self.url = reverse('test-view')
+        print(self.url)
+
+    def test_get_request(self):
+        response = self.client.get(self.url)
+        print(response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), 'Hello from the server!')
